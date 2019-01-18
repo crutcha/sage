1eca2dfbdaa7581bb76dc20d93fd5eca89fe751b
Andrew Crutchfield <andrewjcrutchfield@gmail.com>
2019-01-17 21:53:14 -0500

    more docker-compose stuff

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
 from flask import Flask
+from py2neo import Graph
+import os
+
 app = Flask(__name__)
+
+DATABASE_URL = os.environ.get('NEO4J_HOST', 'bolt://localhost:7687')
+DATABASE_USER = os.environ.get('NEO4J_USERNAME')
+DATABASE_PASSWORD = os.environ.get('NEO4J_PASSWORD')
+
+print(f"URL: {DATABASE_URL}")
+print(f"USER: {DATABASE_USER}")
+print(f"PASSWORD: {DATABASE_PASSWORD}")
+
+graph = Graph(DATABASE_URL, auth=(DATABASE_USER, DATABASE_PASSWORD))
 
 @app.route('/')
 def hello_world():
