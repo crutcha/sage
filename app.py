b19d67a5fe43bd9f38d58b3d88fcbfd28778c3b9
Andrew Crutchfield <andrewjcrutchfield@gmail.com>
2019-01-19 17:33:20 -0500

    some basic bootstrap/css for device listing, we can do the rest later

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask import render_template
 from py2neo import Graph
 import os
+import sys
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 DATABASE_URL = os.environ.get('NEO4J_HOST', 'bolt://localhost:7687')
 DATABASE_USER = os.environ.get('NEO4J_USERNAME')
 DATABASE_PASSWORD = os.environ.get('NEO4J_PASSWORD')
 
-print(f"URL: {DATABASE_URL}")
-print(f"USER: {DATABASE_USER}")
-print(f"PASSWORD: {DATABASE_PASSWORD}")
-
+print(f"DATABASE_URL: {DATABASE_URL}")
 graph = Graph(DATABASE_URL, auth=(DATABASE_USER, DATABASE_PASSWORD))
 
 @app.route('/')
-def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+def device_list():
+    devices = list(graph.nodes.match("Device"))
+    return render_template('devices.html', devices=devices)
 
+@app.route('/bootstraptest')
+def bootstrap_test():
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
