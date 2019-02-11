cb89268fa0b9b4418b6ad9952097e779d9dafd61
Andrew Crutchfield <andrewjcrutchfield@gmail.com>
2019-02-10 23:51:42 -0500

    working through dynamic importer

--- eidetic/providers/__init__.py
+++ eidetic/providers/__init__.py
@@ -9,15 +9,11 @@ import inspect
 import pkgutil
 import importlib
 
-#import pdb; pdb.set_trace()
-
-base_module = None
 base_classes = []
-concrete_classes = []
 for (finder, name, ispkg) in pkgutil.iter_modules([os.path.dirname(__file__)]):
     imported_module = importlib.import_module("." + name, package="providers")
 
-    if name == 'base':
+    if name == "base":
         to_check = inspect.getmembers(imported_module)
 
         for member in to_check:
@@ -25,12 +21,29 @@ for (finder, name, ispkg) in pkgutil.iter_modules([os.path.dirname(__file__)]):
                 base_classes.append(member)
             else:
                 del member
-        
+
+        del to_check
+
     else:
         mod_classes = inspect.getmembers(imported_module)
 
-        for member in mod_classes:
-            if member[0] == "QFX":
-                import pdb; pdb.set_trace()
+        for mem in mod_classes:
+            # Can't combine these conditionals apparently...
+            if inspect.isclass(mem[1]) and any(
+                [issubclass(mem[1], c[1]) for c in base_classes]
+            ):
+                # issubclass with return true when called upon itself,
+                # but we can compare this with base classes to make
+                # sure we aren't exporting a concrete implementation
+                if mem not in base_classes:
+                    setattr(sys.modules[__name__], mem[0], mem[1])
+            else:
+                del mem
+        del mod_classes
 
-import pdb; pdb.set_trace()
+    del imported_module
+
+# Export and cleanup
+for c in base_classes:
+    del c
+del Path, os, sys, inspect, pkgutil, importlib, base_classes
